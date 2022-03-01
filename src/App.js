f7327fb411b7e070127ad06ad9a241b22bf35474
Isaac De La Cruz <89318065+Sirkickassalot@users.noreply.github.com>
2022-02-28 20:58:16 -0800

    added two useStates to save user input in to an empty array

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
+import { useState } from 'react'
 
 function App() {
   const fakeTasks = ['Eat Dinner', 'Do Laundry', 'Go to the gym', ]
+
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState('')
+
+  const addTaskHandler = () => {
+    console.log('clicked add task button')
+    console.log(task)
+  }
+
     return (
       <>
         <h2>TODO LIST APP</h2>
-        <input type='text' style={{ outline: 'none'}}/>
-        <button> Add Task </button>
-        {fakeTasks.map(task =>(
+        <input 
+          type='text' 
+          style={{ outline: 'none'}} 
+          value={task} 
+          onChange={e => setTask(e.target.value)}
+        />
+        <button onClick = {addTaskHandler}> Add Task </button>
+        {tasks.map(task =>(
           <h2>{task}</h2>
         ))}
       </>
